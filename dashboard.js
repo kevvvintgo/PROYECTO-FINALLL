ce3ab14891a8d93b53418ff5c04c5618c1c94633
kevvvintgo <kevintgov18@gmail.com>
2025-12-02 22:56:26 -0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,10 @@
 const token = localStorage.getItem ("token");
 if (!token){
     alert("No estas logueado!!");
-    window.location.href = "login.html";
+    window.location.href = "index.html";
 }
 function logout() {
     localStorage.removeItem("token");
-    window.location.href = "login.html";
+    window.location.href = "index.html";
 }
+
