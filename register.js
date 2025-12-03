7c2a12dddd4f84ff16d208a5cee04c37b473d058
kevvvintgo <kevintgov18@gmail.com>
2025-12-02 22:53:51 -0600

    Add files via upload

---
+++ register.js
@@ -0,0 +1,28 @@
+function registrar() {
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const password2 = document.getElementById("password2").value;
+
+    if (password !== password2) {
+        alert("Las contraseñas no coinciden");
+        return;
+    }
+
+    fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password }) 
+    })
+    .then(res => res.json())
+    .then(data => {
+        alert(data.message);
+
+        if (data.ok) {
+            window.location.href = "index.html";
+        }
+    })
+    .catch(err => console.error("Error al registrar:", err));
+}
